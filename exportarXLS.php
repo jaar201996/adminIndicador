d796aa94cb469baee38c9fe4d3e5cc4183756558
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-03-20 11:54:17 -0500

    Update exportarXLS.php

--- exportarXLS.php
+++ exportarXLS.php
@@ -15,40 +15,15 @@
 
         </tr>
 <?php
-  header('Content-type:application/xls');
-  header('Content-Disposition: attachment; filename=dominio.xls');
-  include("conexion.php");
-  $date1 = date("Y-m-d", strtotime($_POST['date1']));
-  if(is_null($date1)){
-      $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio order by c.fecactual");
-      if(pg_num_rows ($result)>0){
-        while($row = pg_fetch_assoc($result)){
-		  echo '
-				<tr>
-								
-					<td>'.$row['fecactual'].'</td>
-				    <td>'.$row['nombredominio'].'</td>
-					<td>'.$row['owner'].'</td>
-					<td>'.$row['nombreusuario'].'</td>
-					<td>'.$row['numedident'].'</td>
-					<td>'.$row['numedcident'].'</td>
-					<td>'.$row['edccatalog'].'</td>
-					<td>'.$row['ednccatalog'].'</td>
-					<td>'.$row['rndefinidas'].'</td>
-					<td>'.$row['rnimplactejec'].'</td>
-					<td>'.$row['rndesact'].'</td>
-					<td>'.$row['edtrazacatalog'].'</td>
-				    <td>'.$row['edtrazafueracatalog'].'</td>
-				</tr>';
-        	}
-	    }else{
-	       echo '<tr><td colspan="8">No hay datos.</td></tr>';
-	    }
-
-  }else{
-  	 $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio where c.fecactual='$date1' order by c.fecactual");
-	  if(pg_num_rows ($result)>0){
-	      while($row = pg_fetch_assoc($result)){
+   if(ISSET($_POST['enviar'])){
+      header('Content-type:application/xls');
+	  header('Content-Disposition: attachment; filename=dominio.xlsx');
+	  include("conexion.php");
+	  $date1 = date("Y-m-d", strtotime($_POST['date1']));
+	  if(is_null($date1)){
+	      $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio order by c.fecactual");
+	      if(pg_num_rows ($result)>0){
+	        while($row = pg_fetch_assoc($result)){
 			  echo '
 					<tr>
 									
@@ -66,10 +41,37 @@
 						<td>'.$row['edtrazacatalog'].'</td>
 					    <td>'.$row['edtrazafueracatalog'].'</td>
 					</tr>';
-		  }
-	    }else{
-	       echo '<tr><td colspan="8">No hay datos con filtro.</td></tr>';
-	    }
-  }
+	        	}
+		    }else{
+		       echo '<tr><td colspan="8">No hay datos.</td></tr>';
+		    }
+
+	  }else{
+	  	 $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio where c.fecactual='$date1' order by c.fecactual");
+		  if(pg_num_rows ($result)>0){
+		      while($row = pg_fetch_assoc($result)){
+				  echo '
+						<tr>
+										
+							<td>'.$row['fecactual'].'</td>
+						    <td>'.$row['nombredominio'].'</td>
+							<td>'.$row['owner'].'</td>
+							<td>'.$row['nombreusuario'].'</td>
+							<td>'.$row['numedident'].'</td>
+							<td>'.$row['numedcident'].'</td>
+							<td>'.$row['edccatalog'].'</td>
+							<td>'.$row['ednccatalog'].'</td>
+							<td>'.$row['rndefinidas'].'</td>
+							<td>'.$row['rnimplactejec'].'</td>
+							<td>'.$row['rndesact'].'</td>
+							<td>'.$row['edtrazacatalog'].'</td>
+						    <td>'.$row['edtrazafueracatalog'].'</td>
+						</tr>';
+			  }
+		    }else{
+		       echo '<tr><td colspan="8">No hay datos.</td></tr>';
+		    }
+	  }
+   }
 ?>
 </table>
