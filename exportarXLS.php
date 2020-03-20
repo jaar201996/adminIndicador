47f8cfdaee7d1083bba2a0f223d52d0a8d18dd5c
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-03-20 16:32:33 -0500

    Update exportarXLS.php

--- exportarXLS.php
+++ exportarXLS.php
@@ -1,11 +1,12 @@
 <?php
    if(ISSET($_POST['enviar'])){
-      header('Content-type:application/xls');
-	  header('Content-Disposition: attachment; filename=dominio.xls');
+      
 	  include("conexion.php");
 	  $date1 = date("Y-m-d", strtotime($_POST['date1']));
 	  $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio where c.fecactual='$date1' order by c.fecactual");
 	  if(pg_num_rows ($result)>0){
+	        header('Content-type:application/xls');
+	        header('Content-Disposition: attachment; filename=dominio.xls');
 	        while($row = pg_fetch_assoc($result)){
 			  echo '
 					<tr>		
