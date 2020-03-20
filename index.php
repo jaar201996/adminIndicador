22629a925139a977be783f222b43aa55972dd457
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-03-20 17:14:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -84,33 +84,32 @@ include("conexion.php");
 				</tr>';
                  }
             }else{
-                     $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio order by c.fecactual");
-        if(pg_num_rows ($result)>0){
-	      while($row = pg_fetch_assoc($result)){
-			  echo '
-				<tr>			
-			                <td>'.$row['fecactual'].'</td>
-				        <td>'.$row['nombredominio'].'</td>
-					<td>'.$row['owner'].'</td>
-					<td>'.$row['nombreusuario'].'</td>
-					<td>'.$row['numedident'].'</td>
-					<td>'.$row['numedcident'].'</td>
-					<td>'.$row['edccatalog'].'</td>
-					<td>'.$row['ednccatalog'].'</td>
-					<td>'.$row['rndefinidas'].'</td>
-					<td>'.$row['rnimplactejec'].'</td>
-					<td>'.$row['rndesact'].'</td>
-					<td>'.$row['edtrazacatalog'].'</td>
-				    <td>'.$row['edtrazafueracatalog'].'</td>
-				</tr>';
-                 }
-            }else{
-               echo '<tr><td colspan="8">No hay datos.</td></tr>';
-           }
-	    }
-      }
-
-	  if(ISSET($_POST['search'])){
+                 $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio order by c.fecactual");
+		        if(pg_num_rows ($result)>0){
+			      while($row = pg_fetch_assoc($result)){
+					  echo '
+						<tr>			
+					                <td>'.$row['fecactual'].'</td>
+						        <td>'.$row['nombredominio'].'</td>
+							<td>'.$row['owner'].'</td>
+							<td>'.$row['nombreusuario'].'</td>
+							<td>'.$row['numedident'].'</td>
+							<td>'.$row['numedcident'].'</td>
+							<td>'.$row['edccatalog'].'</td>
+							<td>'.$row['ednccatalog'].'</td>
+							<td>'.$row['rndefinidas'].'</td>
+							<td>'.$row['rnimplactejec'].'</td>
+							<td>'.$row['rndesact'].'</td>
+							<td>'.$row['edtrazacatalog'].'</td>
+						    <td>'.$row['edtrazafueracatalog'].'</td>
+						</tr>';
+		                 }
+		            }else{
+		               echo '<tr><td colspan="8">No hay datos.</td></tr>';
+		           }
+	        }
+      }else{
+      	if(ISSET($_POST['search'])){
 	    $date1 = date("Y-m-d", strtotime($_POST['date1']));
 	    $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio where c.fecactual='$date1' order by c.fecactual");
 	    if(pg_num_rows ($result)>0){
@@ -159,7 +158,7 @@ include("conexion.php");
             }else{
                echo '<tr><td colspan="8">No hay datos.</td></tr>';
            }
-        }
+        }}	  
 ?>
 			</table>
 		       
