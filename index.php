e26f6704d54becba6b36693f78e245e55310aceb
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-03-20 16:47:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -87,29 +87,6 @@ include("conexion.php");
                echo '<tr><td colspan="8">No hay datos.</td></tr>';
            }
         }else{
-         $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio order by c.fecactual");
-          header('Content-type:application/xls');
-	  header('Content-Disposition: attachment; filename=dominio.xls');
-	 while($row = pg_fetch_assoc($result)){
-           echo '
-	         <tr>						
-			      <td>'.$row['fecactual'].'</td>
-			      <td>'.$row['nombredominio'].'</td>
-		              <td>'.$row['owner'].'</td>
-			      <td>'.$row['nombreusuario'].'</td>
-		              <td>'.$row['numedident'].'</td>
-			      <td>'.$row['numedcident'].'</td>
-			      <td>'.$row['edccatalog'].'</td>
-		              <td>'.$row['ednccatalog'].'</td>
-		              <td>'.$row['rndefinidas'].'</td>
-			      <td>'.$row['rnimplactejec'].'</td>
-		              <td>'.$row['rndesact'].'</td>
-			      <td>'.$row['edtrazacatalog'].'</td>
-			      <td>'.$row['edtrazafueracatalog'].'</td>
-		</tr>';
-    }
-  }
-	  			
 	  if(ISSET($_POST['search'])){
 	    $date1 = date("Y-m-d", strtotime($_POST['date1']));
 	    $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio where c.fecactual='$date1' order by c.fecactual");
@@ -155,7 +132,7 @@ include("conexion.php");
 			      <td>'.$row['edtrazafueracatalog'].'</td>
 		</tr>';
     }
-  }
+  }}
 ?>
 			</table>
 		       
