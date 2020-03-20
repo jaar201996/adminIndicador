f25a33d4009ec6da0ef851c0163fd4d6b291aa36
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-03-20 12:29:08 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,11 +1,3 @@
 	<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand visible-xs-block visible-sm-block" href="">Inicio</a>
-			</div>
+		
 	</div>
