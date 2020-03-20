46408d946d0c69ffe0772d429f24bf5de7f20f3c
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-03-20 13:12:15 -0500

    Update exportarXLS.php

--- exportarXLS.php
+++ exportarXLS.php
@@ -4,13 +4,11 @@
 	  header('Content-Disposition: attachment; filename=dominio.xls');
 	  include("conexion.php");
 	  $date1 = date("Y-m-d", strtotime($_POST['date1']));
-	  if('$date1'=='aaaa/mm/dd'){
-	      $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio where c.fecactual='$date1' order by c.fecactual");
-	      if(pg_num_rows ($result)>0){
+	  $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio where c.fecactual='$date1' order by c.fecactual");
+	  if(pg_num_rows ($result)>0){
 	        while($row = pg_fetch_assoc($result)){
 			  echo '
-					<tr>
-									
+					<tr>		
 						<td>'.$row['fecactual'].'</td>
 					    <td>'.$row['nombredominio'].'</td>
 						<td>'.$row['owner'].'</td>
@@ -28,33 +26,6 @@
 	        	}
 		    }else{
 		       echo '<tr><td colspan="8">No hay datos.</td></tr>';
-		    }
-
-	  }else{
-	  	 $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio where c.fecactual='$date1' order by c.fecactual");
-		  if(pg_num_rows ($result)>0){
-		      while($row = pg_fetch_assoc($result)){
-				  echo '
-						<tr>
-										
-							<td>'.$row['fecactual'].'</td>
-						    <td>'.$row['nombredominio'].'</td>
-							<td>'.$row['owner'].'</td>
-							<td>'.$row['nombreusuario'].'</td>
-							<td>'.$row['numedident'].'</td>
-							<td>'.$row['numedcident'].'</td>
-							<td>'.$row['edccatalog'].'</td>
-							<td>'.$row['ednccatalog'].'</td>
-							<td>'.$row['rndefinidas'].'</td>
-							<td>'.$row['rnimplactejec'].'</td>
-							<td>'.$row['rndesact'].'</td>
-							<td>'.$row['edtrazacatalog'].'</td>
-						    <td>'.$row['edtrazafueracatalog'].'</td>
-						</tr>';
-			  }
-		    }else{
-		       echo '<tr><td colspan="8">No hay datos.</td></tr>';
-		    }
-	  }
+	        }
    }
 ?>
