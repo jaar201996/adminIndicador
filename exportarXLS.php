1be008cfbc457e28eef73a2478447d2dd7ff90e2
jaar201996 <32993368+jaar201996@users.noreply.github.com>
2020-03-20 12:47:30 -0500

    Update exportarXLS.php

--- exportarXLS.php
+++ exportarXLS.php
@@ -1,26 +1,10 @@
-<table order="1">
-        <tr>
-             <th>Fecha</th>
-             <th>Owner</th>
-             <th>Custodio de Información</th>
-             <th>ED identificados</th>
-             <th>EDC identificados</th>
-             <th>EDC en el Catalog</th>
-             <th>EDNC en el Catalog</th>
-             <th>RN Definidas</th>
-             <th>RN Activas </th>
-             <th>RN Desactivadas</th>
-             <th>ED con Traza</th>
-             <th>ED con Traza en el Catalog</th>
-
-        </tr>
 <?php
    if(ISSET($_POST['enviar'])){
       header('Content-type:application/xls');
 	  header('Content-Disposition: attachment; filename=dominio.xls');
 	  include("conexion.php");
 	  $date1 = date("Y-m-d", strtotime($_POST['date1']));
-	  if(is_null($date1)){
+	  if('$date1'=='aaaa/mm/dd'){
 	      $result = pg_query($dbconn, "SELECT a.*, b.nombreusuario,c.* from dominio  a join usuario b on a.codusuario = b.codusuario join indicador c on a.coddominio = c.coddominio order by c.fecactual");
 	      if(pg_num_rows ($result)>0){
 	        while($row = pg_fetch_assoc($result)){
@@ -74,4 +58,3 @@
 	  }
    }
 ?>
-</table>
